3865a32775baf1a02fbc8e7a2ebfea02d7c91ee9
while1cry <while1cry@163.com>
2025-07-08 02:30:21 +0800

    (core) Update plugin main class

--- src/main/java/net/kamiland/ultimatehub/UltimateHub.java
+++ src/main/java/net/kamiland/ultimatehub/UltimateHub.java
@@ -1,27 +1,40 @@
 package net.kamiland.ultimatehub;
 
-import org.bukkit.event.Listener;
+import net.kamiland.ultimatehub.manager.ConfigManager;
+import net.kamiland.ultimatehub.manager.ModuleManager;
+import net.kamiland.ultimatehub.manager.PlayerManager;
+import net.kamiland.ultimatehub.manager.ServerManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
 
 public class UltimateHub extends JavaPlugin {
 
+    private Logger logger;
+    private ServerManager serverManager;
+    private ConfigManager configManager;
+    private PlayerManager playerManager;
+    private ModuleManager moduleManager;
+
     @Override
     public void onEnable() {
+        logger = getSLF4JLogger();
 
-    }
+        serverManager = new ServerManager(this);
+        serverManager.load();
 
-    @Override
-    public void onLoad() {
+        configManager = new ConfigManager(this);
+        configManager.load();
 
+        playerManager = new PlayerManager(this);
+        playerManager.loadAll();
+
+        moduleManager = new ModuleManager(this, configManager);
     }
 
     @Override
     public void onDisable() {
-
-    }
-
-    public void registerListener(Listener listener) {
-        getServer().getPluginManager().registerEvents(listener, this);
+        moduleManager.disableAll();
+        playerManager.unloadAll();
     }
 
 }
