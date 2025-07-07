82e2363932725bf05b417406fc33e476083273af
while1cry <while1cry@163.com>
2025-07-08 07:29:36 +0800

    (config) Simplify config module by removing interface abstractions

--- src/main/java/net/kamiland/ultimatehub/manager/ConfigManager.java
+++ src/main/java/net/kamiland/ultimatehub/manager/ConfigManager.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import net.kamiland.ultimatehub.UltimateHub;
 import net.kamiland.ultimatehub.config.PluginConfig;
 import net.kamiland.ultimatehub.config.SpawnConfig;
-import net.kamiland.ultimatehub.config.impl.PluginConfigImpl;
-import net.kamiland.ultimatehub.config.impl.SpawnConfigImpl;
 import org.slf4j.Logger;
 
 /**
@@ -56,9 +54,9 @@ public class ConfigManager {
      */
     public void load() {
         logger.info("Loading config...");
-        pluginConfig = new PluginConfigImpl(plugin);
+        pluginConfig = new PluginConfig(plugin);
         pluginConfig.load();
-        spawnConfig = new SpawnConfigImpl(plugin);
+        spawnConfig = new SpawnConfig(plugin);
         spawnConfig.load();
     }
 
