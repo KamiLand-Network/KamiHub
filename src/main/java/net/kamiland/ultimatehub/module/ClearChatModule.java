8c92e5bd541cb536dee445a79498509ceb8aa68e
while1cry <while1cry@163.com>
2025-07-08 09:37:34 +0800

    Implement clear-chat module

--- src/main/java/net/kamiland/ultimatehub/module/ClearChatModule.java
+++ src/main/java/net/kamiland/ultimatehub/module/ClearChatModule.java
@@ -1,33 +1,63 @@
 package net.kamiland.ultimatehub.module;
 
+import net.kamiland.ultimatehub.UltimateHub;
+import net.kamiland.ultimatehub.manager.ConfigManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class ClearChatModule implements Module {
+public class ClearChatModule implements Module, Listener {
+
+    private final UltimateHub plugin;
+    private final ConfigManager configManager;
+    private boolean enabled = false;
+
+    public ClearChatModule(UltimateHub plugin, ConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+
+        setEnabled(configManager.getPluginConfig().IS_CLEARCHAT_ENABLED);
+    }
 
     @Override
     public String getName() {
-        return "";
+        return "clearchat";
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-
+        if (this.enabled == enabled)
+            return;
+        this.enabled = enabled;
+        setup();
     }
 
     @Override
     public void setup() {
+        // Idk what to do with this...
+    }
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (enabled && getPermission() != null && event.getPlayer().hasPermission(getPermission())) {
+            for (int i = 0; i < 100; i++)
+                event.getPlayer().sendMessage("");
+        }
     }
 
     @Override
     @Nullable
     public String getPermission() {
-        return null;
+        return "ultimatehub.clearchat";
     }
 
     @Override
