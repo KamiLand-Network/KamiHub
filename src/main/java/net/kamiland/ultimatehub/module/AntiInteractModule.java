62541d96b3b16a411d3f3e3cc9618c2a6812af61
while1cry <while1cry@163.com>
2025-07-08 09:36:34 +0800

    Implement anti-interact module

--- src/main/java/net/kamiland/ultimatehub/module/AntiInteractModule.java
+++ src/main/java/net/kamiland/ultimatehub/module/AntiInteractModule.java
@@ -1,27 +1,62 @@
 package net.kamiland.ultimatehub.module;
 
+import net.kamiland.ultimatehub.UltimateHub;
+import net.kamiland.ultimatehub.manager.ConfigManager;
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class AntiInteractModule implements Module {
+public class AntiInteractModule implements Module, Listener {
+
+    private final UltimateHub plugin;
+    private final ConfigManager configManager;
+    private boolean enabled = false;
+
+    public AntiInteractModule(UltimateHub plugin, ConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+
+        setEnabled(configManager.getPluginConfig().IS_ANTIINTERACT_ENABLED);
+    }
 
     @Override
     public String getName() {
-        return "";
+        return "antiinteract";
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-
+        if (this.enabled == enabled)
+            return;
+        this.enabled = enabled;
+        setup();
     }
 
     @Override
     public void setup() {
+        // Idk what to do with this...
+    }
 
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (enabled) {
+            if (getBypassPermission() != null && event.getPlayer().hasPermission(getBypassPermission())) {
+                if (configManager.getPluginConfig().IS_ANTIINTERACT_CREATIVE_ONLY) {
+                    if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+                        return;
+                } else return;
+            }
+            event.setCancelled(true);
+        }
     }
 
     @Override
@@ -33,7 +68,7 @@ public class AntiInteractModule implements Module {
     @Override
     @Nullable
     public String getBypassPermission() {
-        return null;
+        return "ultimatehub.antiinteract.bypass";
     }
 
 }
