bfb0a604a14c79c26cf33719a6d0f25d008537a3
while1cry <while1cry@163.com>
2025-07-09 12:14:03 +0800

    refactor(boss-bar): replace per-player timers with global scheduler
    - Deprecated individual player update timers

--- src/main/java/net/kamiland/ultimatehub/module/BossBarModule.java
+++ src/main/java/net/kamiland/ultimatehub/module/BossBarModule.java
@@ -3,7 +3,9 @@ package net.kamiland.ultimatehub.module;
 import net.kamiland.ultimatehub.UltimateHub;
 import net.kamiland.ultimatehub.manager.ConfigManager;
 import net.kamiland.ultimatehub.util.MessageUtil;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.bossbar.BossBar;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -20,8 +22,9 @@ public class BossBarModule extends EventModule {
 
     private final UltimateHub plugin;
     private final ConfigManager configManager;
-    private final Map<Player, BossBar> playerBossBars = new HashMap<>();
-    private final Map<Player, BukkitTask> playerTasks = new HashMap<>();
+    private final Map<Player, BossBar> bossBarMap = new HashMap<>();
+    private final Map<Player, Integer> bossBarIndexMap = new HashMap<>();
+    private BukkitTask timerTask;
 
     public BossBarModule(UltimateHub plugin, ConfigManager configManager) {
         super(plugin, "boss-bar");
@@ -33,60 +36,60 @@ public class BossBarModule extends EventModule {
 
     @Override
     public void load() {
-        plugin.getServer().getOnlinePlayers().forEach(this::setBossBar);
-    }
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            bossBarMap.put(player, null);
+            bossBarIndexMap.put(player, 0);
+        }
 
-    @Override
-    public void unload() {
-        playerTasks.values().forEach(BukkitTask::cancel);
-        playerTasks.clear();
-        playerBossBars.keySet().forEach(player -> player.hideBossBar(playerBossBars.get(player)));
-        playerBossBars.clear();
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        setBossBar(event.getPlayer());
-    }
-
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        if (playerBossBars.containsKey(event.getPlayer()))
-            event.getPlayer().hideBossBar(playerBossBars.get(event.getPlayer()));
-        if (playerTasks.containsKey(event.getPlayer()))
-            playerTasks.get(event.getPlayer()).cancel();
-        playerBossBars.remove(event.getPlayer());
-        playerTasks.remove(event.getPlayer());
-    }
-
-    private void setBossBar(Player player) {
-        if (isEnabled()) {
-            BossBar bossBar = BossBar.bossBar(
-                    MessageUtil.getMessage(player, configManager.getPluginConfig().BOSSBAR_MESSAGES[0]),
-                    configManager.getPluginConfig().BOSSBAR_PROGRESS,
-                    BossBar.Color.valueOf(configManager.getPluginConfig().BOSSBAR_COLOR),
-                    BossBar.Overlay.valueOf(configManager.getPluginConfig().BOSSBAR_OVERLAY));
-            BukkitTask playerTask = new BukkitRunnable() {
-                int i = 0;
-                @Override
-                public void run() {
-                    if (++i >= configManager.getPluginConfig().BOSSBAR_MESSAGES.length)
-                        i = 0;
-                    if (player.hasPermission(getPermission())) {
+        timerTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : bossBarMap.keySet()) {
+                    if (!player.hasPermission(getPermission())) {
+                        if (bossBarMap.get(player) != null)
+                            player.hideBossBar(bossBarMap.get(player));
+                        return;
+                    }
+                    BossBar bossBar = bossBarMap.get(player);
+                    int i = bossBarIndexMap.get(player);
+                    if (bossBar != null) {
                         bossBar.name(MessageUtil.getMessage(player, configManager.getPluginConfig().BOSSBAR_MESSAGES[i]));
                         bossBar.progress(configManager.getPluginConfig().BOSSBAR_PROGRESS);
                         bossBar.color(BossBar.Color.valueOf(configManager.getPluginConfig().BOSSBAR_COLOR));
                         bossBar.overlay(BossBar.Overlay.valueOf(configManager.getPluginConfig().BOSSBAR_OVERLAY));
-                        player.showBossBar(bossBar);
                     } else {
-                        player.hideBossBar(bossBar
+                        bossBar = BossBar.bossBar(
+                                MessageUtil.getMessage(player, configManager.getPluginConfig().BOSSBAR_MESSAGES[i]),
+                                configManager.getPluginConfig().BOSSBAR_PROGRESS,
+                                BossBar.Color.valueOf(configManager.getPluginConfig().BOSSBAR_COLOR),
+                                BossBar.Overlay.valueOf(configManager.getPluginConfig().BOSSBAR_OVERLAY)
                         );
                     }
+                    player.showBossBar(bossBar);
+                    if (++ i >= configManager.getPluginConfig().BOSSBAR_MESSAGES.length)
+                        i = 0;
+                    bossBarMap.put(player, bossBar);
+                    bossBarIndexMap.put(player, i);
                 }
-            }.runTaskTimer(plugin, 0, configManager.getPluginConfig().BOSSBAR_INTERVAL);
-            playerBossBars.put(player, bossBar);
-            playerTasks.put(player, playerTask);
-        }
+            }
+        }.runTaskTimer(plugin, 0, configManager.getPluginConfig().BOSSBAR_INTERVAL);
+    }
+
+    @Override
+    public void unload() {
+        timerTask.cancel();
+        bossBarMap.forEach(Audience::hideBossBar);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        bossBarMap.put(event.getPlayer(), null);
+        bossBarIndexMap.put(event.getPlayer(), 0);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        bossBarMap.remove(event.getPlayer());
     }
 
     @Override
