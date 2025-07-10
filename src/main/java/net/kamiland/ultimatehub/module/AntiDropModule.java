eb03fb1c0318dd15eaf707fad86531c5bae7d033
while1cry <while1cry@163.com>
2025-07-10 08:13:53 +0800

    (feat) Implement AntiDropModule

---
+++ src/main/java/net/kamiland/ultimatehub/module/AntiDropModule.java
@@ -0,0 +1,52 @@
+package net.kamiland.ultimatehub.module;
+
+import net.kamiland.ultimatehub.UltimateHub;
+import net.kamiland.ultimatehub.manager.ConfigManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AntiDropModule extends EventModule {
+
+    private final UltimateHub plugin;
+    private final ConfigManager configManager;
+
+    public AntiDropModule(UltimateHub plugin, ConfigManager configManager) {
+        super(plugin, "anti-drop");
+        this.plugin = plugin;
+        this.configManager = configManager;
+
+        setEnabled(configManager.getPluginConfig().IS_ANTIDROP_ENABLED);
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void unload() {
+
+    }
+
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
+        if (isEnabled() && ! event.getPlayer().hasPermission(getBypassPermission())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    @Nullable
+    public String getPermission() {
+        return null;
+    }
+
+    @Override
+    @NotNull
+    public String getBypassPermission() {
+        return "ultimatehub.anti-drop.bypass";
+    }
+
+}
