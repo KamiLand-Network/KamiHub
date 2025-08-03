9e358d37df78f56a3f47822731877ea4d34ec273
while1cry <while1cry@163.com>
2025-08-03 19:58:06 +0800

    refactor: refactor ModuleManager

--- src/main/java/net/kamiland/kamihub/manager/ModuleManager.java
+++ src/main/java/net/kamiland/kamihub/manager/ModuleManager.java
@@ -7,45 +7,46 @@ import net.kamiland.kamihub.module.Module;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ModuleManager {
 
+    private final KamiHub plugin;
     private final LinkedHashMap<String, Module> modules = new LinkedHashMap<>();
-    private final HashMap<Module, Boolean> moduleEnabled = new HashMap<>();
 
     public ModuleManager(KamiHub plugin) {
-        ModuleConfig config = plugin.getConfigManager().getModuleConfig();
-        put(new ActionBarModule(plugin), config.IS_ACTIONBAR_ENABLED);
-        put(new AgreementModule(plugin), config.IS_AGREEMENT_ENABLED);
-        put(new AntiAttackModule(plugin), config.IS_ANTIATTACK_ENABLED);
-        put(new AntiBreakModule(plugin), config.IS_ANTIBREAK_ENABLED);
-        put(new AntiDamageModule(plugin), config.IS_ANTIDAMAGE_ENABLED);
-        put(new AntiDropModule(plugin), config.IS_ANTIDROP_ENABLED);
-        put(new AntiHungerModule(plugin), config.IS_ANTIHUNGER_ENABLED);
-        put(new AntiUseModule(plugin), config.IS_ANTIUSE_ENABLED);
-        put(new AntiPickupModule(plugin), config.IS_ANTIPICKUP_ENABLED);
-        put(new AntiPlaceModule(plugin), config.IS_ANTIPLACE_ENABLED);
-        put(new AntiProjectileModule(plugin), config.IS_ANTIPROJECTILE_ENABLED);
-        put(new BossBarModule(plugin), config.IS_BOSSBAR_ENABLED);
-        put(new BroadcastModule(plugin), config.IS_BROADCAST_ENABLED);
-        put(new ClearChatModule(plugin), config.IS_CLEARCHAT_ENABLED);
-        put(new FlyModule(plugin), config.IS_FLY_ENABLED);
-        put(new InventoryModule(plugin), config.IS_INVENTORY_ENABLED);
-        put(new JQMessageModule(plugin), config.IS_JQMESSAGE_ENABLED);
-        put(new PotionEffectModule(plugin), config.IS_POTIONEFFECT_ENABLED);
-        put(new SpawnModule(plugin), config.IS_SPAWN_ENABLED);
-        put(new VoidTeleportModule(plugin), config.IS_VOIDTELEPORT_ENABLED);
+        this.plugin = plugin;
+
+        put(new ActionBarModule(plugin));
+        put(new AgreementModule(plugin));
+        put(new AntiAttackModule(plugin));
+        put(new AntiBreakModule(plugin));
+        put(new AntiDamageModule(plugin));
+        put(new AntiDropModule(plugin));
+        put(new AntiHungerModule(plugin));
+        put(new AntiUseModule(plugin));
+        put(new AntiPickupModule(plugin));
+        put(new AntiPlaceModule(plugin));
+        put(new AntiProjectileModule(plugin));
+        put(new BossBarModule(plugin));
+        put(new BroadcastModule(plugin));
+        put(new ClearChatModule(plugin));
+        put(new FlyModule(plugin));
+        put(new InventoryModule(plugin));
+        put(new JQMessageModule(plugin));
+        put(new PotionEffectModule(plugin));
+        put(new SpawnModule(plugin));
+        put(new VoidTeleportModule(plugin));
     }
 
     public void load() {
-        moduleEnabled.forEach((module, enabled) -> {
-            if (enabled) module.setEnabled(true);
-        });
+        enableByConfig();
     }
 
     @Nullable
@@ -60,6 +61,17 @@ public class ModuleManager {
                 .orElseThrow(() -> new IllegalArgumentException("Module \"" + name + "\" does not exist"));
 
         entry.getValue().setEnabled(true);
+    }
+
+    public void enableByConfig() {
+        ConfigurationSection section = plugin.getConfigManager().getModuleConfig().getConfig();
+        section = Objects.requireNonNull(section).getConfigurationSection("modules");
+        Objects.requireNonNull(section);
+        for (String name : modules.keySet()) {
+            if (section.getBoolean(name + ".enabled")) {
+                enable(name);
+            }
+        }
     }
 
     public void disable(String name) {
@@ -93,9 +105,8 @@ public class ModuleManager {
         return modules.keySet().stream().anyMatch(name::equalsIgnoreCase);
     }
 
-    private void put(Module module, boolean enabled) {
+    private void put(Module module) {
         modules.put(module.getName(), module);
-        moduleEnabled.put(module, enabled);
     }
 
 }
