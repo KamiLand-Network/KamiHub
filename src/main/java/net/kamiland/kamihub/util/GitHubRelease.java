a9015dc3efa785009e584f74cdc1a19d699708d7
while1cry <while1cry@163.com>
2025-08-03 19:39:19 +0800

    chore: add NotNull annotation

--- src/main/java/net/kamiland/kamihub/util/GitHubRelease.java
+++ src/main/java/net/kamiland/kamihub/util/GitHubRelease.java
@@ -1,9 +1,12 @@
 package net.kamiland.kamihub.util;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.ZonedDateTime;
 
 public record GitHubRelease(String tag, String name, String body, ZonedDateTime publishedAt, boolean prerelease) {
 
+    @NotNull
     @Override
     public String toString() {
         return "Release[" + tag + (prerelease ? " (pre)" : "") + "]";
